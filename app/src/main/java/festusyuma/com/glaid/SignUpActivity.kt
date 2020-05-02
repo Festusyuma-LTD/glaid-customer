f08e403374ce91ea72243dad0ff18e06f8284ef9
dafom <ayomidechollom@gmail.com>
2020-05-02 16:39:10 +0100

    one time password screen

--- app/src/main/java/festusyuma/com/glaid/SignUpActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/SignUpActivity.kt
@@ -1,12 +1,19 @@
 package festusyuma.com.glaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+    }
+
+    fun signUpMethod(view: View) {
+        var signUpIntent = Intent(this, OneTimePasswordActivity::class.java)
+        startActivity(signUpIntent)
     }
 }
