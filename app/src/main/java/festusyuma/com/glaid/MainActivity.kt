999273f8d904a5c915f749c0d60242ffce7e5291
dafom <ayomidechollom@gmail.com>
2020-05-02 15:47:31 +0100

    sign up page with assets

--- app/src/main/java/festusyuma/com/glaid/MainActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/MainActivity.kt
@@ -1,12 +1,20 @@
 package festusyuma.com.glaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun signUpWithMail(view: View){
+        var signUpMailIntent = Intent(this, SignUpActivity::class.java)
+        startActivity(signUpMailIntent)
     }
 }
