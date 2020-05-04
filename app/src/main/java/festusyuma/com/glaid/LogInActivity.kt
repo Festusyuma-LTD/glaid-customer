6fd897267ffe5d4a6bf73f627df6302daeee3024
dafom <ayomidechollom@gmail.com>
2020-05-04 18:08:16 +0100

    forgot password page

--- app/src/main/java/festusyuma/com/glaid/LogInActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/LogInActivity.kt
@@ -1,12 +1,19 @@
 package festusyuma.com.glaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LogInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+    }
+
+    fun forgotPasswordMethod(view: View) {
+        var forgotPasswordIntent = Intent(this, ForgotPasswordActivity::class.java)
+        startActivity(forgotPasswordIntent)
     }
 }
