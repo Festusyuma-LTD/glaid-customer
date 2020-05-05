80040c8416f2eae7031f7f9689b6b28dd86a88df
dafom <ayomidechollom@gmail.com>
2020-05-05 11:14:30 +0100

    changes on forgot password main page

--- app/src/main/java/festusyuma/com/glaid/ForgotPasswordActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/ForgotPasswordActivity.kt
@@ -1,12 +1,26 @@
 package festusyuma.com.glaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
-
+    var otpChoice: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+
+        radioGroupForgotPassword.setOnCheckedChangeListener { group, checkedId ->
+            if (checkedId === R.id.emailId){
+                otpChoice = "email"
+            } else if (checkedId === R.id.phoneId){
+                otpChoice = "Phone"
+            }
+        }
+    }
+    fun getOtpTypeMethod(view: View){
+//        getOtpIntent = Intent()
     }
 }
