3e524ef72396996d2bd66e297d387ea3eee79465
dafom <ayomidechollom@gmail.com>
2020-05-05 14:58:50 +0100

    phone and email password otp

--- app/src/main/java/festusyuma/com/glaid/ForgotPasswordActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/ForgotPasswordActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -16,11 +17,17 @@ class ForgotPasswordActivity : AppCompatActivity() {
             if (checkedId === R.id.emailId){
                 otpChoice = "email"
             } else if (checkedId === R.id.phoneId){
-                otpChoice = "Phone"
+                otpChoice = "phone"
             }
         }
     }
     fun getOtpTypeMethod(view: View){
-//        getOtpIntent = Intent()
+        if (otpChoice !== "") {
+            var getOtpIntent = Intent(this, ForgotPasswordOtpActivity::class.java)
+            getOtpIntent.putExtra(EXTRA_FORGOT_PASSWORD_CHOICE, otpChoice)
+            startActivity(getOtpIntent)
+        } else {
+            Toast.makeText(applicationContext, "please select one of the options", Toast.LENGTH_LONG).show()
+        }
     }
 }
