d239353e1aac29544ab7067c5afc212d426de5f4
dafom <ayomidechollom@gmail.com>
2020-05-18 17:21:41 +0100

    splasg screen

---
+++ app/src/main/java/festusyuma/com/glaid/SplashActivity.kt
@@ -0,0 +1,31 @@
+package festusyuma.com.glaid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    private val runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
