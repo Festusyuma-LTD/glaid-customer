909852aaee016dc64d1b3597a7ff0b27ba88cd0c
dafom <ayomidechollom@gmail.com>
2020-05-27 18:18:02 +0100

    changes on map

--- app/src/main/java/festusyuma/com/glaid/DetailsFragment.kt
+++ app/src/main/java/festusyuma/com/glaid/DetailsFragment.kt
@@ -1,40 +1,57 @@
 package festusyuma.com.glaid
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_maps.*
 import kotlinx.android.synthetic.main.fragment_details.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
-//    override fun onCreateView(
+    //    override fun onCreateView(
 //        inflater: LayoutInflater, container: ViewGroup?,
 //        savedInstanceState: Bundle?
 //    ): View? {
 //        // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_root, container, false)
 //    }
-override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    // toggle button
-    orderBtnOne.setOnClickListener(){
-        toggleOrderButtonOne()
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        quantityBtn.setOnClickListener {
+
+            requireActivity().supportFragmentManager.beginTransaction()
+//            .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left)// set animation
+                .setCustomAnimations(
+                    R.anim.slide_up,
+                    R.anim.slide_down,
+                    R.anim.slide_up,
+                    R.anim.slide_down
+                )
+                .replace(R.id.framelayoutFragment, QuantityFragment.quantityInstance())
+                .addToBackStack(null)
+                .commit()
+        }
+        // toggle button
+        orderBtnOne.setOnClickListener() {
+            toggleOrderButtonOne()
+        }
+        orderBtnTwo.setOnClickListener() {
+            toggleOrderButtonTwo()
+        }
     }
-    orderBtnTwo.setOnClickListener(){
-        toggleOrderButtonTwo()
-    }
-}
-    fun toggleOrderButtonOne (){
+    fun toggleOrderButtonOne() {
         orderBtnTwo.isChecked = false
     }
-    fun toggleOrderButtonTwo (){
+
+    fun toggleOrderButtonTwo() {
         orderBtnOne.isChecked = false
     }
+
     companion object {
         fun newInstance() = DetailsFragment()
 
--- app/src/main/java/festusyuma/com/glaid/MapsActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/MapsActivity.kt
@@ -1,12 +1,22 @@
 package festusyuma.com.glaid
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.graphics.Color
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.app.ActivityCompat
@@ -14,23 +24,31 @@ import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.activity_maps.*
-
+import kotlinx.android.synthetic.main.drawer_header.*
+import android.provider.Settings;
+import kotlin.properties.Delegates
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    val PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    var longitude by Delegates.notNull<Double>()
+    var latitude by Delegates.notNull<Double>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        // tint removal for drawer items
         nav_view.itemIconTintList = null;
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -57,73 +75,146 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        setupPermissions()
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        // Instantiates a new CircleOptions object and defines the center and radius
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    this, R.raw.uber_map_style
+                )
+            )
+            if (!success) {
+                Log.e("FragmentActivity.TAG", "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("FragmentActivity.TAG", "Can't find style. Error: ", e)
+        }
+        getLastLocation()
+    }
+    fun setUserLocationOnMap(latitude: Double, longitude: Double){
+        var userLocation = LatLng(latitude, longitude)
         val circleOptions = CircleOptions()
-            .center(sydney)
+            .center(userLocation)
             .radius(500.0)
             .strokeWidth(0.0f)
-            .strokeColor(R.color.AppPrimaryColor)
-            .fillColor(R.color.AppPrimaryColor)
+            .strokeColor(Color.argb(50, 78, 0, 124))
+            .fillColor(Color.argb(50, 78, 0, 124))
             .clickable(true); // In meters
         val mapIcon = AppCompatResources.getDrawable(this, R.drawable.customlocation)!!.toBitmap()
         mMap.addMarker(
-            MarkerOptions().position(sydney).title("Marker in Sydney")
+            MarkerOptions().position(userLocation).title("Marker in Sydney")
                 .icon(BitmapDescriptorFactory.fromBitmap(mapIcon))
         )
-//        mMap.addMarker(
-//            MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromBitmap(mapIcon))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15.0f))
-        // Instantiates a new CircleOptions object and defines the center and radius
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15.0f))
         mMap.addCircle(circleOptions)
+
     }
+
+    // This will check if the user has turned on location from the setting
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    // check if user has given permission
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+    // request permission from user if they've no granted the permission
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    // This method is called when a user Allow or Deny our requested permissions. So it will help us to move forward if the permissions are granted
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // Granted. Start getting the location information
+            }
+        } else {
+            // request the permission again
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        latitude = location.latitude
+                        longitude = location.longitude
+//                        mMap.isMyLocationEnabled = true
+//                        mMap.uiSettings.isMyLocationButtonEnabled = true
+                        setUserLocationOnMap(latitude, longitude)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+    // record the location information in runtime to prevent location from being null
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    // callback for previous locations
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            latitude = mLastLocation.latitude
+            longitude = mLastLocation.longitude
+        }
+    }
+    // edit profile intent
+    fun editProfileIntent(view: View){
+        val editIntent = Intent(this, EditProfileActivity::class.java)
+        startActivity(editIntent)
+    }
+
     fun toggleDrawerClick(view: View) {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
+            rating.setRating(4.5f)
         } else {
             drawerLayout.openDrawer(GravityCompat.START);
+            rating.setRating(4.5f)
         }
     }
-    private fun setupPermissions() {
-        val permission = ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            Log.i("ACCESS_FINE_LOCATION", "Permission to use location denied")
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            ) {
-                val builder = AlertDialog.Builder(this)
-                builder.setMessage("Permission to access the Device Location is required for this Map to work.")
-                    .setTitle("Permission required")
-
-                builder.setPositiveButton(
-                    "OK"
-                ) { dialog, id ->
-                    Log.i("ACCESS_FINE_LOCATION", "Clicked")
-                    makeRequest()
-                }
-                val dialog = builder.create()
-                dialog.show()
-            } else {
-                makeRequest()
-            }
-        }
+    fun goToUserLocation(view: View){
+        var userLocation = LatLng(this.latitude, this.longitude)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 17.0f))
     }
-    private fun makeRequest() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            1
-        )
-    }
-
 }
