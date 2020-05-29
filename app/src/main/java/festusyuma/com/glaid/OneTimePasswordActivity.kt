89a6752be2466e9d6c566c803289eeb69521f176
Festus Agboma <festusyuma@gmail.com>
2020-05-30 00:58:20 +0100

    temp fix

--- app/src/main/java/festusyuma/com/glaid/OneTimePasswordActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/OneTimePasswordActivity.kt
@@ -8,15 +8,10 @@ import festusyuma.com.glaid.requestdto.UserRegistrationRequest
 
 class OneTimePasswordActivity : AppCompatActivity() {
 
-    private val userRequest = intent.getSerializableExtra("userRequest") as UserRegistrationRequest
+//    private val userRequest = intent.getSerializableExtra("userRequest") as UserRegistrationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_time_password)
-    }
-
-    fun completeRegistration() {
-        val queue = Volley.newRequestQueue(this)
-
     }
 }
