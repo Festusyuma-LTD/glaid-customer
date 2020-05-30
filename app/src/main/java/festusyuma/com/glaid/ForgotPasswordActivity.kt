02ae5dba76d49a6bb4f903e2fcc4182bf655f3c3
Festus Agboma <festusyuma@gmail.com>
2020-05-30 12:44:25 +0100

    forgot password view fix

--- app/src/main/java/festusyuma/com/glaid/ForgotPasswordActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/ForgotPasswordActivity.kt
@@ -13,10 +13,10 @@ class ForgotPasswordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
-        radioGroupForgotPassword.setOnCheckedChangeListener { group, checkedId ->
-            if (checkedId === R.id.emailId){
+        radioGroupForgotPassword.setOnCheckedChangeListener { _, checkedId ->
+            if (checkedId == R.id.emailRadio){
                 otpChoice = "email"
-            } else if (checkedId === R.id.phoneId){
+            } else if (checkedId == R.id.telRadio){
                 otpChoice = "phone"
             }
         }
