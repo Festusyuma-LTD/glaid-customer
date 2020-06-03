675c9276fabebb1599ecb3d0fb39cbcf570a6f0a
Festus Agboma <festusyuma@gmail.com>
2020-06-03 04:11:14 +0100

    greeting shows actual name

--- app/src/main/java/festusyuma/com/glaid/RootFragment.kt
+++ app/src/main/java/festusyuma/com/glaid/RootFragment.kt
@@ -1,10 +1,15 @@
 package festusyuma.com.glaid
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import festusyuma.com.glaid.helpers.getFirst
+import festusyuma.com.glaid.model.User
+import kotlinx.android.synthetic.main.activity_maps.*
 import kotlinx.android.synthetic.main.fragment_root.*
 
 /**
@@ -14,6 +19,14 @@ class RootFragment : Fragment(R.layout.fragment_root) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        val sharedPref = this.activity?.getSharedPreferences("cached_data", Context.MODE_PRIVATE)
+        val userJson = sharedPref?.getString("userDetails", "null")
+
+        if (userJson != null) {
+            val user = gson.fromJson(userJson, User::class.java)
+            greeting.text = getString(R.string.home_greeting_intro_text).format(user.fullName.getFirst())
+        }
 
         dieselBtn.setOnClickListener{
             //todo get diesel details
--- app/src/main/java/festusyuma/com/glaid/helpers/Extensions.kt
+++ app/src/main/java/festusyuma/com/glaid/helpers/Extensions.kt
@@ -4,3 +4,5 @@ import java.util.*
 
 fun String.capitalizeWords(): String =
     split(" ").joinToString(" ") { it.first().toUpperCase() + it.substring(1) }
+
+fun String.getFirst(): String = split(" ")[0]
