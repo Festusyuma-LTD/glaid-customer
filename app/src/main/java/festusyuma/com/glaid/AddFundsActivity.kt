f31118ab29f61ca756f334b6b1fcac9a44547243
dafom <ayomidechollom@gmail.com>
2020-06-05 16:11:22 +0100

    loader on add funds page

--- app/src/main/java/festusyuma/com/glaid/AddFundsActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/AddFundsActivity.kt
@@ -1,18 +1,28 @@
 package festusyuma.com.glaid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_funds.*
 
 class AddFundsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_funds)
+        addFundsBtn.setOnClickListener {
+            startAnim()
+            loadingCoverConstraint.visibility = View.VISIBLE
+        }
+//        startAnim()
     }
     fun paymentIntentMethod(view: View) {
         var paymentIntent = Intent(this, PaymentActivity::class.java)
         startActivity(paymentIntent)
+    }
+    fun startAnim() {
+        avi.show()
+        // or avi.smoothToShow();
     }
 }
