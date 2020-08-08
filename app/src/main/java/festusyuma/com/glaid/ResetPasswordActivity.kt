3511d14ce3fdc2669de8d4192f361e7fd0669a7b
Festus Agboma <festusyuma@gmail.com>
2020-08-08 14:15:36 +0100

    reset password

--- app/src/main/java/festusyuma/com/glaid/ResetPasswordActivity.kt
+++ app/src/main/java/festusyuma/com/glaid/ResetPasswordActivity.kt
@@ -13,12 +13,12 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.wang.avi.AVLoadingIndicatorView
 import festusyuma.com.glaid.helpers.Api
+import festusyuma.com.glaid.request.Authentication
 import festusyuma.com.glaid.requestdto.PasswordResetRequest
 import org.json.JSONObject
 
 class ResetPasswordActivity : AppCompatActivity() {
 
-    private var operationRunning = false
     private lateinit var passwordResetRequest: PasswordResetRequest
 
     private lateinit var passwordInput: EditText
@@ -26,17 +26,9 @@ class ResetPasswordActivity : AppCompatActivity() {
     private lateinit var verifyPasswordInput: EditText
     private lateinit var verifyPasswordInputError: TextView
 
-    private lateinit var loadingCover: ConstraintLayout
-    private lateinit var loadingAvi: AVLoadingIndicatorView
-    private lateinit var errorMsg: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
-
-        loadingCover = findViewById(R.id.loadingCoverConstraint)
-        loadingAvi = loadingCover.findViewById(R.id.avi)
-        errorMsg = findViewById(R.id.errorMsg)
 
         passwordInput = findViewById(R.id.passwordInput)
         passwordInputError = findViewById(R.id.passwordInputError)
@@ -47,38 +39,13 @@ class ResetPasswordActivity : AppCompatActivity() {
     }
 
     fun resetPassword(view: View) {
-        if (!operationRunning) {
-            setLoading(true)
-            passwordResetRequest.newPassword = passwordInput.text.toString()
-
-            if (!hasError()) {
-                val queue = Volley.newRequestQueue(this)
-                val resetJsonObject = JSONObject(gson.toJson(passwordResetRequest))
-
-                val request = JsonObjectRequest(
-                    Request.Method.POST,
-                    Api.RESET_PASSWORD,
-                    resetJsonObject,
-                    Response.Listener {
-                        response ->
-                        if (response.getInt("status") == 200) {
-                            val intent = Intent(this, LogInActivity::class.java)
-                            startActivity(intent)
-                            finishAffinity()
-                        }else showError(response.getString("message"))
-
-                        setLoading(false)
-                    },
-                    Response.ErrorListener {
-                        response ->
-                        response.printStackTrace()
-                        showError("An error occurred")
-                        setLoading(false)
-                    }
-                )
-
-                queue.add(request)
-            }else setLoading(false)
+        passwordResetRequest.newPassword = passwordInput.text.toString()
+        if (!hasError()) {
+            Authentication(this).resetPassword(passwordResetRequest) {
+                val intent = Intent(this, LogInActivity::class.java)
+                startActivity(intent)
+                finishAffinity()
+            }
         }
     }
 
@@ -107,26 +74,5 @@ class ResetPasswordActivity : AppCompatActivity() {
         }else verifyPasswordInputError.text = ""
 
         return error
-    }
-
-    private fun setLoading(loading: Boolean) {
-        if (loading) {
-            loadingCover.visibility = View.VISIBLE
-            loadingAvi.show()
-            operationRunning = true
-        }else {
-            loadingCover.visibility = View.GONE
-            loadingAvi.hide()
-            operationRunning = false
-        }
-    }
-
-    private fun showError(msg: String) {
-        errorMsg.text = msg
-        errorMsg.visibility = View.VISIBLE
-    }
-
-    fun hideError(view: View) {
-        errorMsg.visibility = View.INVISIBLE
     }
 }
